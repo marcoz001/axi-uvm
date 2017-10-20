ed4ea00c6efa58a05de79c9fba304c225ce5ba1f
Matt Dew <marcoz@osource.org>
2017-10-20 16:13:11 -0600

    write address and read address simple coverage collection

--- tb/axi_coveragecollector.svh
+++ tb/axi_coveragecollector.svh
@@ -20,7 +20,7 @@
 /*! \class axi_coveragecollector
  *  \brief Collects coverage
  *
- * or will, currently does nothing.
+ * Write Address and Read Address simple coverage (burst_size, burst_type and awlen)
  */
 class axi_coveragecollector extends uvm_subscriber #(axi_seq_item);
 
@@ -28,7 +28,7 @@ class axi_coveragecollector extends uvm_subscriber #(axi_seq_item);
 
   covergroup aw_cg with function sample(axi_seq_item item);
   //option.name = "aw";
-  option.per_instance = 1;
+  option.per_instance = 0;
 
     BURSTSIZE : coverpoint item.burst_size {
       bins e_1BYTE    = {axi_pkg::e_1BYTE};
@@ -41,10 +41,44 @@ class axi_coveragecollector extends uvm_subscriber #(axi_seq_item);
       bins e_128BYTES = {axi_pkg::e_128BYTES};
     }
 
+    BURSTTYPE : coverpoint item.burst_type {
+      bins e_FIXED   = {axi_pkg::e_FIXED};
+      bins e_INCR    = {axi_pkg::e_INCR};
+      bins e_WRAP    = {axi_pkg::e_WRAP};
+    }
+
     AWLEN : coverpoint item.awlen[3:0];
 
+    /* awcache, awlock, awprot, awqos here someday */
 
   endgroup: aw_cg
+
+  covergroup ar_cg with function sample(axi_seq_item item);
+  //option.name = "aw";
+  option.per_instance = 0;
+
+    BURSTSIZE : coverpoint item.burst_size {
+      bins e_1BYTE    = {axi_pkg::e_1BYTE};
+      bins e_2BYTES   = {axi_pkg::e_2BYTES};
+      bins e_4BYTES   = {axi_pkg::e_4BYTES};
+      bins e_8BYTES   = {axi_pkg::e_8BYTES};
+      bins e_16BYTES  = {axi_pkg::e_16BYTES};
+      bins e_32BYTES  = {axi_pkg::e_32BYTES};
+      bins e_64BYTES  = {axi_pkg::e_64BYTES};
+      bins e_128BYTES = {axi_pkg::e_128BYTES};
+    }
+
+    BURSTTYPE : coverpoint item.burst_type {
+      bins e_FIXED   = {axi_pkg::e_FIXED};
+      bins e_INCR    = {axi_pkg::e_INCR};
+      bins e_WRAP    = {axi_pkg::e_WRAP};
+    }
+
+    ARLEN : coverpoint item.arlen[3:0];
+
+    /* awcache, awlock, awprot, awqos here someday */
+
+  endgroup: ar_cg
 
 
 
@@ -55,11 +89,13 @@ endclass : axi_coveragecollector
 
 /*! \brief Constructor
  *
- * allocates covergroup */
+ * allocates covergroups */
 function axi_coveragecollector::new(string name="axi_coveragecollector", uvm_component parent=null);
    super.new(name, parent);
 
       aw_cg = new();
+      ar_cg = new();
+
 endfunction : new
 
 /*! \brief covergroup sampling
@@ -68,5 +104,16 @@ endfunction : new
  */
 function void axi_coveragecollector::write(axi_seq_item t);
   `uvm_info(this.get_type_name(), $sformatf("%s", t.convert2string()), UVM_HIGH)
-  aw_cg.sample(t);
+
+  case (t.cmd)
+    axi_uvm_pkg::e_WRITE : begin
+       aw_cg.sample(t);
+      end
+
+      axi_uvm_pkg::e_READ  : begin
+        ar_cg.sample(t);
+      end
+
+   endcase
+
 endfunction : write
